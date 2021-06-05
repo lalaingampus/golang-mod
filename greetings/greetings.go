b94cd6e8c57a3204d7a0ce09f7bed562f1e552f5
lalaingampus <bottlewar@gmail.com>
2021-06-05 20:41:05 +0700

    error handling

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,14 @@
 package greetings
 
-import "fmt"
-
-func Hello(name string) string {
- message := fmt.Sprintf("Hi, %v. Welcome!", name)
- return message
+import (
+	"fmt"
+	"errors"
+)
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+ 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+ 	return message, nil
 }
 
